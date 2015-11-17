18e07039928107936ee041817856142e91d2ee81
rodgerju <rodgerju@oregonstate.edu>
2015-11-16 22:25:23 -0500

    Got local host logged in.  Removed blank function, fixed Credential Call

--- test.py
+++
@@ -1,9 +0,0 @@
-import MySQLdb
-import MySQLdb.cursors as cursors
-
-conn = MySQLdb.connect("localhost","cs","password","testing")
-cursor = conn.cursor()
-cursor.execute("Select * from users")
-for row in cursor:
-	print row
-
